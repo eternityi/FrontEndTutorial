ebb11e8891113902e29b8b41cfbe4b7b3f6d9696
qing <qing@mbp.com>
2018-08-09 16:57:58 +0800

    js

---
+++ JavaScript/test.js
@@ -0,0 +1,21 @@
+// 注释
+let x;
+
+x = 3 + y;
+console.log(x);
+
+foo(x, y);
+obj.bar(3);
+
+if (x === 0) {
+    x = 123;
+}
+
+/**
+ * @param  {string} a a
+ * @param  {string} b b
+ * @return {string} a+b
+ */
+function foo(a, b) {
+    return a + b;
+}
